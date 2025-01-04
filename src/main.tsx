acbe14749218a5eaaac0953c1cae50455d0fb56a
Neysel <neyseltheblack@gmail.com>
2025-01-05 00:05:59 +0400

    second commit

---
+++ src/main.tsx
@@ -0,0 +1,69 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import ErrorPage from "./error-page";
+import Root, { loader as rootLoader, action as rootAction, } from "./routes/root";
+import EditContact, {
+  action as editAction,
+} from "./routes/edit";
+import { action as destroyAction } from "./routes/destroy";
+import Contact, {
+  loader as contactLoader,
+  action as contactAction,
+} from "./routes/contact";
+import { Outlet } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import './index.css'
+import Index from "./routes/index";
+import App from './App.tsx'
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
+    children: [
+      {errorElement: <ErrorPage />,
+      children: [
+        { index: true, element: <Index /> },
+        {
+          path: "contacts/:contactId",
+          element: <Contact />,
+          loader: contactLoader,
+          action: contactAction,
+        },
+        {
+          path: "contacts/:contactId",
+          element: <Contact />,
+          loader: contactLoader,
+          action: contactAction,
+        },  
+        {
+          path: "contacts/:contactId/edit",
+          element: <EditContact />,
+          loader: contactLoader,
+          action: editAction,
+        },
+        {
+          path: "contacts/:contactId/destroy",
+          action: destroyAction,
+          // errorElement: <div>Oops! There was an error.</div>,
+        },
+      ],
+    },
+      
+    ],
+  },
+]);
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+     <RouterProvider router={router} />
+  </StrictMode>,
+)
+
